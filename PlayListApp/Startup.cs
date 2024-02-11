a84afb82d0039cd41e0a167c47096b08b44d2642
Ahmed Mohsen Taha <Ahmed.Mohsen@nebrastech.com>
2024-02-11 16:54:32 +0200

    Add project files.

---
+++ PlayListApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PlayListApp.Startup))]
+namespace PlayListApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
